f929e23ad562a583628a7d31648f07752d24c35e
Jaehong Kang <sinoru@me.com>
2024-08-13 20:33:47 +0900

    Adds Sodium.Data Random Implementation

---
+++ Sources/Sodium/Sodium+Data_Random.swift
@@ -0,0 +1,18 @@
+//
+//  Sodium+Data_Random.swift
+//
+//
+//  Created by Jaehong Kang on 8/13/24.
+//
+
+import Clibsodium
+
+extension Sodium.Data {
+    public static func random(count: Int) -> Self {
+        let data = Array<UInt8>(unsafeUninitializedCapacity: count) { buffer, initializedCount in
+            randombytes_buf(buffer.baseAddress!, count)
+        }
+
+        return self.init(data)
+    }
+}
