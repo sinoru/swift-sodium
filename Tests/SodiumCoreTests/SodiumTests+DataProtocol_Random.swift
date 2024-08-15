7d4f8e2f9d43b3d6e4ffca266b69696257e069de
Jaehong Kang <sinoru@me.com>
2024-08-15 14:34:53 +0900

    Fix build error

--- Tests/SodiumCoreTests/SodiumTests+DataProtocol_Random.swift
+++ Tests/SodiumCoreTests/SodiumTests+DataProtocol_Random.swift
@@ -11,9 +11,8 @@ import XCTest
 extension SodiumTests {
     func testRandom() throws {
         let randomSize = DataSize(byteCount: (1..<UInt8.max).randomElement()!)
-        let randomData = Array<UInt8>.random(count: randomSize.byteCount)
-        let emptyData = Array<UInt8>(repeating: 0, count: randomSize.byteCount)
-        
+        let randomData = try Sodium.Data.random(count: randomSize.byteCount)
+        let emptyData = Sodium.Data(repeating: 0, count: randomSize.byteCount)
 
         XCTAssertFalse(randomData.isEmpty)
         XCTAssertEqual(randomData.size, randomSize)
